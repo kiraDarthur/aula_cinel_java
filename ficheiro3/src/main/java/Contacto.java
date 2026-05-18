e9fc3394110c03388c8a42e06ca01c931045681d
Arthur Fonseca <arthurandrade8713@gmail.com>
2026-05-18 21:42:28 +0100

    Upload clean copy to aula_cinel_java repo

---
+++ ficheiro3/src/main/java/Contacto.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author F40784
+ */
+public class Contacto {
+    
+}
---
+++ projeto3/src/main/java/Aluno.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author F40784
+ */
+public class Aluno {
+    private int numero;
+    private String nome;
+    private int nota;
+    private String localidade;
+
+    public Aluno(int numero, String nome, int nota, String localidade) {
+        this.numero = numero;
+        this.nome = nome;
+        this.nota = nota;
+        this.localidade = localidade;
+    }
+}
